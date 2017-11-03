5668a8f44206c59af292ba2cb5a9a482bd168db7
emileerlarson <emileerlarson@gmail.com>
2017-11-02 21:19:59 -0600

    emilee

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //this will hold stuff
+console.log("hi")
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC5JMKi1ecW4nKIx55XGEUqtJRUuNisYSo",
+    authDomain: "what2do-fe0ef.firebaseapp.com",
+    databaseURL: "https://what2do-fe0ef.firebaseio.com",
+    projectId: "what2do-fe0ef",
+    storageBucket: "what2do-fe0ef.appspot.com",
+    messagingSenderId: "254261395910"
+  };
+  firebase.initializeApp(config);
